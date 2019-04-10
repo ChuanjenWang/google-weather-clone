68f0ee81f7b3c001b5490e93207f896f5e8744ff
Spencer.Wang <chuanjen.wang@gmail.com>
2019-04-10 11:08:07 +0800

    Updates

---
+++ precache-manifest.1a4e9b427de4b4560ce0841fd089f8aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0763f04006a0e26772b1faeabfe79409",
+    "url": "/google-weather-clone/static/media/sunny.0763f040.png"
+  },
+  {
+    "revision": "39a2ab675fbbbed3952b",
+    "url": "/google-weather-clone/static/js/runtime~main.350080c5.js"
+  },
+  {
+    "revision": "1153212985c4c2b74483",
+    "url": "/google-weather-clone/static/js/main.19cdfab9.chunk.js"
+  },
+  {
+    "revision": "5b3df7564a06804a8e4a",
+    "url": "/google-weather-clone/static/js/2.3ccf9383.chunk.js"
+  },
+  {
+    "revision": "1153212985c4c2b74483",
+    "url": "/google-weather-clone/static/css/main.3de821ad.chunk.css"
+  },
+  {
+    "revision": "9b92e7fe78f799428922aab7d8097c13",
+    "url": "/google-weather-clone/index.html"
+  }
+];
